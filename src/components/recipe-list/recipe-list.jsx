c7c93b6609f9d908b94e079fd8f6363bf69c8572
lolyparty <ololadeolayiwola01@gmail.com>
2021-01-05 02:39:51 +0100

    loading spinner and clearing interface on search

--- src/components/recipe-list/recipe-list.jsx
+++ src/components/recipe-list/recipe-list.jsx
@@ -8,12 +8,19 @@ const Recipelist = ({search})=>{
 
     const [results, searchResults] = React.useState([])
 
+    const [isLoading, setLoading] = React.useState(false)
+
+    
+
     React.useEffect(()=>{
         const getRecipe = async () =>{
             try{
                 if(search !== ''){
+                    searchResults([])
+                    setLoading(true)
                     const data = await axios.get(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${search}&key=${Api_Key}`)
-                searchResults(data.data.data.recipes)
+                    searchResults(data.data.data.recipes)
+                    setLoading(false)
                 }
                 
             } catch{
@@ -25,6 +32,7 @@ const Recipelist = ({search})=>{
     },[search])
 
     return <div className="recipe_list">
+            {isLoading && <div className="loading"></div>}
             {results.map(recipe=><RecipeListItem key={recipe.id} {...recipe}/>)}
         </div>
 }
