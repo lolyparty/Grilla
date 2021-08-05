f104da02a55cc55a3829803212ef03525badedcf
lolyparty <ololadeolayiwola01@gmail.com>
2021-08-05 23:03:54 +0100

    pagination logic

--- src/components/recipe-list/recipe-list.jsx
+++ src/components/recipe-list/recipe-list.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './recipe-list.css'
 import {RecipeListItem} from './recipe-list-item'
 import axios from 'axios'
+import leftArrow from './left-arrow.svg'
+import rightArrow from './right-arrow.svg'
 
 const Recipelist = ({search})=>{
 
@@ -9,16 +11,22 @@ const Recipelist = ({search})=>{
 
     const [isLoading, setLoading] = React.useState(false)
 
+    const [curPage, setPage] = React.useState(1)
+
+    const [numberofPages, setnumberofPages] = React.useState(0)
+
 
     React.useEffect(()=>{
         const getRecipe = async () =>{
             try{
                 if(search !== ''){
+                    setnumberofPages(0)
                     searchResults([])
                     setLoading(true)
                     const data = await axios.get(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${search}&key=${process.env.REACT_APP_API_KEY}`)
                     searchResults(data.data.data.recipes)
                     setLoading(false)
+                    setnumberofPages(Math.ceil(data.data.data.recipes.length/10))
                 }
                 
             } catch{
@@ -29,10 +37,28 @@ const Recipelist = ({search})=>{
         getRecipe()
     },[search])
 
-    return <div className="p-1 flex flex-wrap justify-evenly">
+    const prevPage = (e)=>{
+        e.preventDefault()
+        setPage(curPage - 1)
+    }
+
+    const nextPage = (e)=>{
+        e.preventDefault()
+        setPage(curPage + 1)
+    }
+
+    return <div>
+        <div className="p-1 flex flex-wrap justify-evenly">
             {isLoading && <div className="w-16 h-16 rounded-full border-transparent border-4 loading my-2 mx-auto animate-spin"></div>}
-            {results.map(recipe=><RecipeListItem key={recipe.id} {...recipe}/>)}
+            {results.slice(curPage * 10 - 10, curPage * 10).map(recipe=>{
+                return <RecipeListItem key={recipe.id} {...recipe}/>
+            })}
         </div>
+        <div className="p-8 navigationBtns justify-center flex w-full"> 
+        <div className="w-full flex justify-center">{numberofPages === 1 || curPage > 1 ? <button className="flex py-1 px-9 focus:outline-none rounded-xl border-2 border-textblue text-textblue text-lg" onClick={prevPage}><img className="align-middle w-8" src={leftArrow} alt="Prev page" />&nbsp; Prev </button> : null}</div>
+        <div className="w-full flex justify-center">{curPage < numberofPages ? <button className="flex py-1 px-9 focus:outline-none rounded-xl transition duration-1000 border-2 border-textblue text-textblue text-lg" onClick={nextPage}>Next &nbsp;<img className="align-middle w-8" src={rightArrow} alt="Next page" /></button> : null}</div>
+        </div>
+    </div>
 }
 
 export {Recipelist}
