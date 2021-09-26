e6e9a626e0c1701f0bd7d7bf7f12e2b26d50fbaf
lolyparty <ololadeolayiwola01@gmail.com>
2021-09-26 16:34:59 +0100

    fixed images to make them full

--- src/components/recipe-list/recipe-list-item.jsx
+++ src/components/recipe-list/recipe-list-item.jsx
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom'
 const RecipeListItem = ({id,image_url,title,publisher})=>{
     return <Link to={`/${id}`} className="my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl transition duration-1000 transform hover:scale-101">
                     <div className="p-1 flex">
-                        <div className="w-20 h-20 overflow-hidden rounded-full mt-3">
-                            <img src={image_url} alt={title}/>
+                        <div className="w-20 h-20 overflow-hidden rounded-full mt-3 flex justify-center">
+                            <img src={image_url} alt={title} className="w-full"/>
                         </div>
                         <div className="mt-3 ml-3 text-textblue">
                             <h1 className="recipe_name text-xl sm:text-2xl">{title.slice(0,13)}...</h1>
