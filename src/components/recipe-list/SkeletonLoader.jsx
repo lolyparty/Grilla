3670c11e546000192ba95c6180acf70d5b2a9ee0
lolyparty <ololadeolayiwola01@gmail.com>
2021-09-26 16:08:40 +0100

    replaced spinning loader with skeleton loader

---
+++ src/components/recipe-list/SkeletonLoader.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import './recipe-list.css'
+
+
+const SkeletonLoader = (i)=>{
+    return  (<div className="p-1 flex flex-wrap justify-evenly">
+                <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            <div className="p-1 flex my-3 mx-2 shadow-lg bg-white w-96 p-3 rounded-2xl relative overflow-hidden">
+                <div className="skeleton"></div>
+                <div className="w-20 h-20 overflow-hidden rounded-full mt-3 bg-gray-200 ">
+                </div>
+                <div className="mt-5 ml-3">
+                    <p className="text-xl sm:text-2xl bg-gray-200 py-3 px-24 rounded-xl"></p>
+                    <p className="mt-3 inline-block bg-gray-200 py-2 px-16 rounded-lg"></p>
+                </div>
+            </div>
+            </div>)
+}
+
+export {SkeletonLoader}
