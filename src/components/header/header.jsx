0df4e6b313d3a7e90947c06f70eb5dd6c5ec406d
lolyparty <ololadeolayiwola01@gmail.com>
2022-03-12 16:07:33 +0100

    header responsiveness

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,12 +8,12 @@ const Header = ({search, changed,inputValue, keyPress, showLikes, clicked, liked
     
 
 
-    return <div className="header flex flex-wrap my-2 smm:justify-evenly xsm:justify-around mx-auto relative xxsm:my-0 xsm:max-w-lg ">
-            <div className="flex p-4 mdd:px-2  ml-4 smm:ml-0 smm:pl-0 xsm:pr-0.5 xsm:ml-0 xxsm:ml-1 xxsm:px-0 items-center xxsm:w-6/12">
-                <div className="rounded-full flex items-center justify-center w-20 h-20 smm:w-14 smm:h-14 md:w-16 md:h-16 xsm:w-12 xsm:h-12">
+    return <div className="header flex flex-wrap my-2 justify-evenly xsm:justify-around mx-auto relative xxsm:my-0 xsm:max-w-lg ">
+            <div className="flex p-4 mdd:px-2  lg:ml-0 lg:px-2 mdd:pl-0 xsm:pr-0.5 xsm:ml-0 xxsm:ml-1 xxsm:px-0 items-center xxsm:w-6/12">
+                <div className="rounded-full flex items-center justify-center w-20 h-20 lg:w-16 lg:h-16 mdd:w-14 mdd:h-14 md:w-16 md:h-16 xsm:w-12 xsm:h-12">
                     <img src={logo} alt="Grilla Icon" className="icon w-20 h-20"/> 
                 </div>
-                <h1 className="Grilla ml-3 mdd:ml-2 mt-5 smm:mt-2 text-3xl md:text-2xl xsm:text-xl xsm:mt-2">Grilla</h1>
+                <h1 className="Grilla ml-3  lg:ml-2 lg:mt-1 mdd:ml-2 mt-3 mdd:mt-2 text-3xl md:text-2xl xsm:text-xl xsm:mt-2">Grilla</h1>
             </div>
             <Searchrecipe 
                 Search={search}
