2c36d379f641eaef613d8fa0d1ddb398509ea051
lolyparty <ololadeolayiwola01@gmail.com>
2022-04-03 18:08:10 +0100

    updated skeleton loader by adding enough padding

--- src/components/recipe-list/SkeletonLoader.jsx
+++ src/components/recipe-list/SkeletonLoader.jsx
@@ -4,72 +4,72 @@ import './recipe-list.css'
 
 const SkeletonLoader = ()=>{
     return  (<div className="p-3 flex flex-wrap justify-evenly">
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
                 </div>
-                <div className=" skeleton p-3 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
-                    <div className="w-16 h-16 rounded-full mt-3 bg-gray-200 "></div>
-                        <div className="mt-5 ml-2">
+                <div className=" skeleton px-3 py-6 flex  my-3 mx-2 shadow-lg bg-white w-96 rounded-2xl relative overflow-hidden"> 
+                    <div className="w-16 h-16 rounded-full mt-2 bg-gray-200 "></div>
+                        <div className="mt-4 ml-2">
                             <p className="bg-gray-200 py-3 px-20 rounded-xl"></p>
                             <p className="mt-3 inline-block bg-gray-200 py-2 px-12 rounded-lg"></p>
                         </div>
